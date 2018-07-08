d9043247f19078a7cfe33187e040239df590277e
adrianmarino <adrian.marino@restorando.com>
2018-07-07 22:55:48 -0300

    change view point when arrive to extraction zone

--- Source/FPSGame/Private/FPSGameMode.cpp
+++ Source/FPSGame/Private/FPSGameMode.cpp
@@ -2,6 +2,7 @@
 #include "FPSHUD.h"
 #include "FPSCharacter.h"
 #include "UObject/ConstructorHelpers.h"
+#include "Kismet/GameplayStatics.h"
 
 AFPSGameMode::AFPSGameMode() {
 	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Blueprints/BP_Player"));
@@ -12,8 +13,29 @@ AFPSGameMode::AFPSGameMode() {
 
 void AFPSGameMode::CompleteMission(APawn* InstigatorPawn) {
 	if(!InstigatorPawn) return;
+	if(!SpectatingViewpointClass) {
+		UE_LOG(LogTemp, Error, TEXT("SpectatingViewpointClass is nullptr!. Can not change view spectating view target."));
+		return;
+	}
 
 	UE_LOG(LogTemp, Log, TEXT("Mission Completed!"));
 	InstigatorPawn->DisableInput(nullptr);
+
+	TArray<AActor*> SpectatingActors;
+	UGameplayStatics::GetAllActorsOfClass(this, SpectatingViewpointClass, SpectatingActors);
+	if(SpectatingActors.Num() < 0) return;
+
+	AActor* NewViewTarget = SpectatingActors[0];	
+	APlayerController* PlayerController = Cast<APlayerController>(
+		InstigatorPawn->GetController()
+	);
+	if(!PlayerController) return;
+
+	PlayerController->SetViewTargetWithBlend(
+		NewViewTarget,
+		0.5f,
+		EViewTargetBlendFunction::VTBlend_Cubic
+	);
+
 	OnMissionCompleted(InstigatorPawn);
 }
